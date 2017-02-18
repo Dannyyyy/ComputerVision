867dcb91e935ce5ae65d3cb01184d6075330bcb5
danil.shakhov <d.shakhov93@gmail.com>
2017-02-19 00:57:26 +0700

    Image load.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QDir>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QGraphicsPixmapItem>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,17 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    const QString dirs = QFileDialog::getOpenFileName(0, "Выбор изображения...", "", "*.jpg");
+    QImage image(dirs);
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+    QGraphicsItem *pixmap_item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+    scene->addItem(pixmap_item);
+    const int height = image.height();
+    const int width = image.width();
+    ui->graphicsView->fitInView(pixmap_item, Qt::KeepAspectRatio);
 }
